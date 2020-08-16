835516b92dfad79267154308217c5e901b41ca98
Jakob Ackermann <das7pad@outlook.com>
2020-08-16 18:40:21 +0200

    [misc] lazy load the backend implementations

--- src/PersistorFactory.js
+++ src/PersistorFactory.js
@@ -1,23 +1,22 @@
 const Logger = require('logger-sharelatex')
 const { SettingsError } = require('./Errors')
-const GcsPersistor = require('./GcsPersistor')
-const S3Persistor = require('./S3Persistor')
-const FSPersistor = require('./FSPersistor')
-const MigrationPersistor = require('./MigrationPersistor')
 
 function getPersistor(backend, settings) {
   switch (backend) {
     case 'aws-sdk':
     case 's3':
+      const S3Persistor = require('./S3Persistor')
       return new S3Persistor(
         Object.assign({}, settings.s3, { Metrics: settings.Metrics })
       )
     case 'fs':
+      const FSPersistor = require('./FSPersistor')
       return new FSPersistor({
         paths: settings.paths,
         Metrics: settings.Metrics
       })
     case 'gcs':
+      const GcsPersistor = require('./GcsPersistor')
       return new GcsPersistor(
         Object.assign({}, settings.gcs, { Metrics: settings.Metrics })
       )
@@ -43,6 +42,7 @@ module.exports = function create(settings) {
   if (settings.fallback && settings.fallback.backend) {
     const primary = persistor
     const fallback = getPersistor(settings.fallback.backend, settings)
+    const MigrationPersistor = require('./MigrationPersistor')
     persistor = new MigrationPersistor(
       primary,
       fallback,
